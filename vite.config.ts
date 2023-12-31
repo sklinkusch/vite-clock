a40094ce8b18d59cec07b05c68c7ba9336708696
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-12-31 15:32:54 +0100

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: process.env.basename,
+  plugins: [react()],
+  define: {
+    "process.env": process.env
+  },
+  server: {
+    host: true
+  },
+  build: {
+    chunkSizeWarningLimit: 1500
+  },
+  resolve: {
+    alias: {
+      "@assets": resolve(__dirname, "./src/assets"),
+      "@components": resolve(__dirname, "./src/components"),
+      "@constants": resolve(__dirname, "./src/constants"),
+      "@services": resolve(__dirname, "./src/services")
+    }
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    css: false
+  }
+});
